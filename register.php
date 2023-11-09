157f5f349cbe311a0fcf75ce802f1a79f3756441
Mitfort <poszeleznymateusz@gmail.com>
2023-11-09 10:28:56 +0100

    Test1

---
+++ register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    
+    <div class="register-container">
+
+        <form action="" method="post">
+            <fieldset>
+                <legend>
+                    REGISTER
+                </legend>
+            </fieldset>
+            
+            <div class="form-row">
+                <input type="text" name="imie" id="">
+            </div>
+
+            <div class="form-row">
+                <input type="text" name="nazwisko" id="">
+            </div>
+
+            <div class="form-row">
+                <input type="email" name="email" id="">
+            </div>
+
+            <button type="button"> SIGN </button>
+                       
+
+        </form>
+
+    </div>
+
+</body>
+</html>
