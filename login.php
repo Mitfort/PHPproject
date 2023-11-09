234e3cc109ea88aa6f76a058d207893993f770d2
Mitfort <poszeleznymateusz@gmail.com>
2023-11-09 11:58:19 +0100

    Register v1

--- login.php
+++ login.php
@@ -4,11 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     
     <div class="login-container">
+        <form action="" method="post">
+            <fieldset>
+                <legend>
+                    LOGIN
+                </legend>
 
+                <div class="form-row">
+                    <input type="email" name="email" id="">
+                </div>
+
+                <div class="form-row">
+                    <input type="password" name="password" id="">
+                </div>
+
+                <button type="button"> SIGN </button>
+
+            </fieldset>
+
+        </form>
     </div>
 
 </body>
